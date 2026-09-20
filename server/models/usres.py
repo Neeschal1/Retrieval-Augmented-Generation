9db7daae8b467699a9ef19f6b6746ea373da21dd
Neeschal1 <neeschalpok04@gmail.com>
2026-09-20 08:58:54 +0545

    ft: users table creation

---
+++ server/models/usres.py
@@ -0,0 +1,14 @@
+from sqlalchemy import JSON, String, Column, Integer, Boolean
+from pydantic import BaseModel
+from core.database import Base
+
+class User(Base):
+    __tablename__ = "Users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    fullname = Column(String(255))
+    email = Column(String(50), unique=True)
+    username = Column(String(20), unique=True)
+    password = Column(String(20))
+    gender = Column(String(6))
+    isactive = Column(Boolean, default=False)
