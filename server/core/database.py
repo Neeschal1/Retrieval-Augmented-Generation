d3a03b1614580ececb51cef58b4121718829e1d4
Neeschal1 <neeschalpok04@gmail.com>
2026-09-20 08:52:00 +0545

    ft: postgres database configuration

---
+++ server/core/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi import Depends
+from sqlalchemy.ext.declarative import declarative_base
+from typing import Annotated
+from sqlalchemy.orm import Session
+from env_config import Config
+
+DATABASE_URL = f"postgresql://{Config.DB_USERNAME}:{Config.DB_PASSWORD}@{Config.DB_HOST}/{Config.DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind = engine, 
+    autoflush = False, 
+    autocommit = False
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependencies = Annotated[Session, Depends(get_db)]
