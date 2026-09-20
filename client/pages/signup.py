17c1391bcd71262b730707165dbcbfb1fc82fc7b
Neeschal1 <neeschalpok04@gmail.com>
2026-09-20 22:20:48 +0545

    ft: implementation of signup ui component

--- client/pages/signup.py
+++ client/pages/signup.py
@@ -2,13 +2,21 @@ import streamlit as st
 from constants.footer import footer
 
 st.title("📝 Signup")
-st.subheader("New to PookieAI?")
+st.subheader("New to PookieAI? Register a new account in order to begin with...")
 
-email = st.text_input("Email")
-password = st.text_input("Password", type="password")
+fullname = st.text_input("Full Name", placeholder="Enter your full name")
+email = st.text_input("Email", placeholder="Enter your email")
+username = st.text_input("Username", placeholder="Choose a username")
+gender = st.radio("Gender", ["Male", "Female", "Other"], horizontal=True)
+password = st.text_input("Password", type="password", placeholder="Enter your password")
+confirm_password = st.text_input("Password", type="password", placeholder="Confirm your password")
+remember_me = st.checkbox("Remember me :)")
 
-login_btn = st.button("Login", type="primary")
-if login_btn:
-    st.success("Login successful!")
+signup_btn = st.button("Signup", type="primary", use_container_width=True)
+    
+if signup_btn:
+    st.success("Signup successful!")
+
+st.divider()
 
 footer()
