f40c0350e1d5d08b9e149e48569062219912c7d1
Neeschal1 <neeschalpok04@gmail.com>
2026-09-21 18:36:06 +0545

    ft: implementation of jwt setup in fastapi server

--- server/env_config.py
+++ server/env_config.py
@@ -9,3 +9,7 @@ class Config:
     DB_HOST = os.getenv('DB_HOST')
     DB_PORT = os.getenv('DB_PORT')
     DB_NAME = os.getenv('DB_NAME')
+    
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    HASH_ALGORITHM = os.getenv('HASH_ALGORITHM')
+    TOKEN_EXPIRY = os.getenv('TOKEN_EXPIRY')
