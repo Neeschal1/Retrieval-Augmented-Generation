39fd6f14dc3ee2282a5b4b2941b76ea95024edd6
Neeschal1 <neeschalpok04@gmail.com>
2026-09-21 09:33:55 +0545

    ft: definition of database model for uploading document

---
+++ server/models/document.py
@@ -0,0 +1,15 @@
+from sqlalchemy import JSON, String, Column, Integer, Boolean, ForeignKey, Text, DateTime
+from pydantic import BaseModel
+from sqlalchemy.sql import func
+from core.database import Base
+from .users import User 
+
+class Docs(Base):
+    __tablename__ = "Documents"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    userid = Column(Integer, ForeignKey("Users.id"), nullable=False)
+    filename = Column(String(50), nullable=False)
+    filetype = Column(String(10), nullable=False)
+    fullcontent = Column(Text, nullable=False)
+    createdat = Column(DateTime, server_default=func.now(), nullable=False)
