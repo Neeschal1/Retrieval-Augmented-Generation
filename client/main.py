846b6335d06a14f2d88ddb4106988f904400b5ec
Neeschal1 <neeschalpok04@gmail.com>
2026-09-21 09:10:53 +0545

    ft: conversation pathway initialization

--- client/main.py
+++ client/main.py
@@ -5,15 +5,14 @@ st.set_page_config(page_title="PookieAI", page_icon="👧", layout="wide")
 home_page = st.Page("pages/home.py", title="Home", icon="👧")
 login_page = st.Page("pages/login.py", title="Login", icon="🔐")
 signup_page = st.Page("pages/signup.py", title="Signup", icon="📝")
-forgot_password_page = st.Page(
-    "pages/forgotpassword.py", title="ForgotPassword", icon="🔐"
-)
-upload_document = st.Page("pages/document.py", title="UploadDocument", icon="📝")
+forgot_password_page = st.Page("pages/forgotpassword.py", title="ForgotPassword", icon="🔐")
+upload_document_page = st.Page("pages/document.py", title="UploadDocument", icon="📝")
+conversation_page = st.Page("pages/conversation.py", title="Conversation", icon="💬")
 
 pg = st.navigation(
     {
         "": [home_page, login_page, signup_page, forgot_password_page],
-        "protected": [upload_document],
+        "protected": [upload_document_page, conversation_page],
     },
     position="top",
 )
