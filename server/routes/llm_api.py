4e0c0b682303845c38f2e807bf3e45c5803b94c7
Neeschal1 <neeschalpok04@gmail.com>
2026-09-23 09:05:26 +0545

    ft: implementation of creating encrypted api key for storing api key securely in the database

--- server/routes/llm_api.py
+++ server/routes/llm_api.py
@@ -4,27 +4,28 @@ from models.users import User as UserDB
 from schemas.llm import APIKey as APIKeySchema
 from core.database import db_dependencies
 from core.config import get_current_user
+from core.ciphertext import encrypt_data, decrypt_data
 
 llmapi = APIRouter(prefix="/llm", tags=["Api Key"])
 
-
 @llmapi.post("/register-api-key", status_code=status.HTTP_201_CREATED)
-async def create_api_key(
-    db: db_dependencies,
-    user_apikey: APIKeySchema,
-    current_userid: str = Depends(get_current_user),
-):
+async def create_api_key ( db: db_dependencies, user_apikey: APIKeySchema, current_userid: str = Depends(get_current_user),):
     try:
         user = db.query(UserDB).filter(UserDB.id == int(current_userid)).first()
         if not user:
             raise HTTPException(status_code=404, detail={"message": "User not found"})
         
-        # hashed_key = 
+        encrypted_key = encrypt_data(user_apikey.key) 
 
-        apikey = APIKeyDB(userid=user.id, key=user_apikey.key)
+        apikey = APIKeyDB(
+            userid=user.id, 
+            key=encrypted_key
+        )
+        
         db.add(apikey)
         db.commit()
         db.refresh(apikey)
+        
         return {"message": "API Key uploaded successfully :)", "data": apikey}
     
     except Exception as e:
